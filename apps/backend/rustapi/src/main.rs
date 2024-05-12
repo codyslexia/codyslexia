3f4b0860b38eedab94008236cfd6b8983c0f2724
moatorres <44585769+moatorres@users.noreply.github.com>
2024-05-12 02:58:37 -0300

    fix(core): serve rustapi with logging enabled

--- apps/backend/rustapi/src/main.rs
+++ apps/backend/rustapi/src/main.rs
@@ -1,6 +1,7 @@
 use actix_cors::Cors;
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder, Result};
+use actix_web::{get, middleware::Logger, web, App, HttpResponse, HttpServer, Responder, Result};
 use chrono::{DateTime, Utc};
+use env_logger::Env;
 use serde::Serialize;
 
 #[derive(Serialize)]
@@ -38,8 +39,10 @@ async fn not_found() -> Result<HttpResponse> {
 async fn main() -> std::io::Result<()> {
     let addr = match std::env::var("SERVER_HOST") {
         Ok(host) => host,
-        Err(_e) => "127.0.0.1:3000".to_string(),
+        Err(_e) => "0.0.0.0:3000".to_string(),
     };
+
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
 
     let server = HttpServer::new(|| {
         let cors = Cors::default()
@@ -49,6 +52,7 @@ async fn main() -> std::io::Result<()> {
             .max_age(3600);
 
         App::new()
+            .wrap(Logger::default())
             .wrap(cors)
             .service(status)
             .default_service(web::route().to(not_found))
