39b7c8eea0a6a66acbd78281a00e29d66907b2b8
moatorres <44585769+moatorres@users.noreply.github.com>
2024-05-25 09:59:06 -0300

    setup(plugin): update workspace plugin

--- libs/plugins/workspace/src/common/constants.ts
+++ libs/plugins/workspace/src/common/constants.ts
@@ -1,3 +1,3 @@
 export const DEFAULT_REGISTRY = 'ghcr.io'
 export const DEFAULT_ORGANISATION = 'codyslexia'
-export const NODEJS_DEFAULT_IMAGE = 'docker.io/node:22.2.0-alpine3.19'
+export const NODEJS_DEFAULT_IMAGE = 'docker.io/node:20.13.1-alpine3.20'
