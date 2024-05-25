2596ddf03c80ef2e3ed94d7255abe2f3ccae0b62
moatorres <44585769+moatorres@users.noreply.github.com>
2024-05-25 18:11:16 -0300

    setup(repo): remove shared infra

--- libs/shared/infra/src/terraform/gcp/variables.tf
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023-2024 Codyslexia
-# @license MIT
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
-variable "region" {
-  description = "The region for the resources"
-  default     = "us-east1"
-}
